335fc9c6c901967790c775d9f9a3fbd4cff8ad9a
Ankit-Sonavane <83824159+Ankit-Sonavane@users.noreply.github.com>
2021-05-08 22:46:08 +0530

    Add files via upload

---
+++ Lab 2 Assignment/Pattern Set-1/Pattern1.java
@@ -0,0 +1,26 @@
+class Pattern1
+{
+
+public static void main(String args[])
+{
+	for(int i=1; i<=5; i++)			//5
+	{
+		for(int j=1; j<=i; j++)		//12345
+		{
+	 	System.out.print(" "+j);	//1
+		}
+	 	System.out.println();
+	}
+}
+}
+
+/*
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+
+
+*/
